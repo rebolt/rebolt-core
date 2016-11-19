1638dca494f065c01bf548417b5324c15d7bde9c
ikenn <ikenn@nexon.co.kr>
2016-11-19 22:33:13 +0900

    + NotSupportedException

---
+++ src/main/java/io/rebolt/core/exceptions/NotSupportedException.java
@@ -0,0 +1,19 @@
+package io.rebolt.core.exceptions;
+
+import org.apache.logging.log4j.Level;
+
+/**
+ * 지원하지 않는 기능을 호출할 때 발생
+ */
+public final class NotSupportedException extends ReboltException {
+  private static final long serialVersionUID = -1954009198862871085L;
+
+  public NotSupportedException(String message) {
+    super.setMessage(message);
+  }
+
+  @Override
+  protected Level setLogLevel() {
+    return Level.DEBUG;
+  }
+}
