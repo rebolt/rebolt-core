ec0dfd70e83558dbed3a304fbf898216b3c7e76c
ikenn <ikenn@nexon.co.kr>
2017-03-02 21:13:14 +0900

    + Utils, 기본생성사 제거

--- src/main/java/io/rebolt/core/utils/JsonUtil.java
+++ src/main/java/io/rebolt/core/utils/JsonUtil.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MappingJsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 
@@ -15,6 +17,7 @@ import static io.rebolt.core.constants.Constants.STRING_JSON_INITIALIZE;
  *
  * @since 0.1.3
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class JsonUtil {
   private final static JsonFactory jsonFactory = new MappingJsonFactory();
 
