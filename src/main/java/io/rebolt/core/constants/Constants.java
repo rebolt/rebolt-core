c572f0d95f9955cc2b59acbca31872bc242000a2
ikenn <ikenn@nexon.co.kr>
2017-07-23 13:53:42 +0900

    + Constants character 추가

--- src/main/java/io/rebolt/core/constants/Constants.java
+++ src/main/java/io/rebolt/core/constants/Constants.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2017 The Rebolt Framework
+ *
+ * The Rebolt Framework licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package io.rebolt.core.constants;
 
 import lombok.AccessLevel;
@@ -10,21 +27,29 @@ import java.nio.charset.Charset;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
-  public static final String STRING_EMPTY = "";
-  public static final String STRING_BLANK = " ";
-  public static final String STRING_DASH = "-";
-  public static final String STRING_COMMA = ",";
   public static final String STRING_AND = "&";
-  public static final String STRING_EQUAL = "=";
-  public static final String STRING_SHARP = "#";
-  public static final String STRING_QUESTION = "?";
+  public static final String STRING_BLANK = " ";
   public static final String STRING_COLON = ":";
+  public static final String STRING_COMMA = ",";
+  public static final String STRING_DASH = "-";
   public static final String STRING_DOUBLE_COLON = "::";
+  public static final String STRING_EMPTY = "";
+  public static final String STRING_EQUAL = "=";
   public static final String STRING_JSON_INITIALIZE = "{}";
+  public static final String STRING_QUESTION = "?";
+  public static final String STRING_SHARP = "#";
   public static final String STRING_SLASH = "/";
-  public static final CharSequence CHARS_EMPTY = STRING_EMPTY;
 
-  public static final char CHARACTER_SLASH = STRING_SLASH.charAt(0);
+  public static final Character CHARACTER_AND = STRING_AND.charAt(0);
+  public static final Character CHARACTER_BLANK = STRING_BLANK.charAt(0);
+  public static final Character CHARACTER_COLON = STRING_COLON.charAt(0);
+  public static final Character CHARACTER_COMMA = STRING_COMMA.charAt(0);
+  public static final Character CHARACTER_DASH = STRING_DASH.charAt(0);
+  public static final Character CHARACTER_EQUAL = STRING_EQUAL.charAt(0);
+  public static final Character CHARACTER_QUESTION = STRING_QUESTION.charAt(0);
+  public static final Character CHARACTER_SHARP = STRING_SHARP.charAt(0);
+  public static final Character CHARACTER_SLASH = STRING_SLASH.charAt(0);
+  public static final CharSequence CHARS_EMPTY = STRING_EMPTY;
 
   public static final String CHARSET_NAME_DEFAULT = "UTF-8";
   public static final Charset CHARSET_DEFAULT = Charset.forName(CHARSET_NAME_DEFAULT);
